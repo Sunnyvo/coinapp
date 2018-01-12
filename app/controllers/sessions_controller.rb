01ad7a1b4d1d7064e4024b0adefb91f9bcfb5411
sunnyvo <votanphu.2101@gmail.com>
2018-01-12 06:25:47 +0000

    haha

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
     @user = User.where(email: params[:email]).first
     if @user&.valid_password?(params[:password])
       log_in(@user)
+      puts current_user.authentication_token
       respond_to do |format|
       # render json: user.as_json(only: [:email, :authentication_token]), status: :created
         format.json  {render :create,  status: :created}
