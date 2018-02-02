77636b6a3adef3f37983d659bdb0b1b6da6598d6
sunnyvo <votanphu.2101@gmail.com>
2018-02-02 02:45:06 +0000

    test

--- app/workers/chartupdate.rb
+++ app/workers/chartupdate.rb
@@ -1,16 +1,19 @@
 require 'sneakers'
 require 'json'
-require 'sneakers'
-require 'action_cable'
+require "action_cable"
+# require ''
 # require_relative 'config/application'
 class Chartupdate
-
   include Sneakers::Worker
   from_queue 'chartqueue'
+
   def work(msg)
+    puts 'hi1111111111111111111111111111111111111'
     payload = JSON.parse(msg)
-    puts  ActionCable
+    puts 'hi2222222222222222222222222222222222222'
+    binding.pry
     ActionCable.server.broadcast 'prices', data: payload
+    puts '3'
     ack!
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
+  # ActionCable.server.config.logger = Logger.new(nil)
+  # config.action_cable.logger = Logger.new(nil)
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
